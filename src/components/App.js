1e97f973bc1f0ea1572bf1b8ec2cfe13fe468b2f
elva <e.souto.fernandez@gmail.com>
2020-01-27 21:47:22 +0100

    finish api

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "../stylesheet/App.scss";
+import apiData from "../api/api.json";
 import Header from "./Header";
 import Home from "./Home";
 import AboutMe from "./AboutMe";
@@ -7,19 +8,28 @@ import Proyects from "./Proyects";
 import Contact from "./Contact";
 import Footer from "./Footer";
 
-function App() {
-  return (
-    <React.Fragment>
-      <Header />
-      <main>
-        <Home />
-        <AboutMe />
-        <Proyects />
-        <Contact />
-      </main>
-      <Footer />
-    </React.Fragment>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      proyects: apiData
+    };
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <Header />
+        <main>
+          <Home />
+          <AboutMe />
+          <Proyects proyects={this.state.proyects} />
+          <Contact />
+        </main>
+        <Footer />
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
--- src/components/Proyects.js
+++ src/components/Proyects.js
@@ -1,13 +1,35 @@
 import React from "react";
 import "../stylesheet/Proyects.scss";
 
-function Proyects() {
+function Proyects(props) {
   return (
     <React.Fragment>
       <section className="proyects section">
         <div className="section__box">
           <h2 className="section__title">Proyectos</h2>
-          <p className="section__text">Aqui van a ir los proyectos</p>
+          <p className="section__text">
+            Muy "proud" de mis proyectos y ejercicios que he realizado hasta
+            ahora!! :)
+          </p>
+          <ul className="section__list proyectList">
+            {props.proyects.map(proyect => {
+              console.log(proyect.image);
+              return (
+                <li className="proyectList__itemPro" key={proyect.id}>
+                  <img
+                    className="proyectList__imagePro"
+                    src={`${proyect.image}`}
+                    alt=""
+                  />
+                  <h4 className="proyectList__titlePro">{proyect.proyect}</h4>
+                  <p className="proyectList__descriptPro">
+                    {proyect.description}
+                  </p>
+                  <span className="proyectList__datePro">{proyect.date}</span>
+                </li>
+              );
+            })}
+          </ul>
         </div>
       </section>
     </React.Fragment>
