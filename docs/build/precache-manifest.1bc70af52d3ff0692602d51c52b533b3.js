7165af36e4f63aa968384c4c80a65987525a2fc5
elva <e.souto.fernandez@gmail.com>
2020-01-29 17:46:47 +0100

    add new docs

---
+++ docs/build/precache-manifest.1bc70af52d3ff0692602d51c52b533b3.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f2223cdcf339704da3507774ae7a323",
+    "url": "./index.html"
+  },
+  {
+    "revision": "487ebd0153219118f33e",
+    "url": "./static/css/main.29ee9b87.chunk.css"
+  },
+  {
+    "revision": "cf48c58e688c70c646fc",
+    "url": "./static/js/2.77def372.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "./static/js/2.77def372.chunk.js.LICENSE"
+  },
+  {
+    "revision": "487ebd0153219118f33e",
+    "url": "./static/js/main.270e7ad8.chunk.js"
+  },
+  {
+    "revision": "8607f3848100444edd79",
+    "url": "./static/js/runtime-main.e34fff3e.js"
+  },
+  {
+    "revision": "03b5c05cf273e13d02b995ef63b083a3",
+    "url": "./static/media/anonymousBlog.03b5c05c.png"
+  },
+  {
+    "revision": "8fc6db14dd0e75ec5a7210dd3698458c",
+    "url": "./static/media/aweProCards.8fc6db14.png"
+  },
+  {
+    "revision": "7c31090c0c2923915d3b7ce45b4c6571",
+    "url": "./static/media/aweProCardsReact.7c31090c.png"
+  },
+  {
+    "revision": "916499f63785af3a326ab9cd674bb8bd",
+    "url": "./static/media/blueWood.916499f6.jpg"
+  },
+  {
+    "revision": "ef1c0343edf6acd8f598f3899005e01d",
+    "url": "./static/media/buscadorSeries.ef1c0343.png"
+  },
+  {
+    "revision": "1d795191504efcd758badb39cff5e289",
+    "url": "./static/media/codeLikeAGirl.1d795191.png"
+  },
+  {
+    "revision": "1a3f6751f2fc0cf09dcba2e0f889ee4e",
+    "url": "./static/media/elva-cv.1a3f6751.jpeg"
+  },
+  {
+    "revision": "80963d2962ebbd1d1c489a12cc8cbae6",
+    "url": "./static/media/elvaGrey.80963d29.jpeg"
+  },
+  {
+    "revision": "c7f3e7e1c78286ed367515018dfb0fb8",
+    "url": "./static/media/fairyBackground.c7f3e7e1.png"
+  },
+  {
+    "revision": "013e894a2d27d0f9a0020957767d6366",
+    "url": "./static/media/heartBackground.013e894a.png"
+  },
+  {
+    "revision": "1c2f9f9ddfe4f1f20fbf83584c771da3",
+    "url": "./static/media/listaDePokémon.1c2f9f9d.png"
+  },
+  {
+    "revision": "e63829dcfa3d8e43c3902242aa16feeb",
+    "url": "./static/media/rick&morty.e63829dc.png"
+  }
+]);
